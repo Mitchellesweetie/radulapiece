f9c719c94c133631f6a6297c8cf06de1f711f21e
Mitchellesweetie <mitchellengoli@gmail.com>
2023-11-25 09:55:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mysql = require('mysql')
+const port = 3000
+const path = require('path')
+
+
+//app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'medease',
+})
+
+db.connect((err) => {
+  if (err) {
+    console.log('Database connection error: ' + err.message);
+  } else {
+    console.log('Connected to the database');
+  }
+})
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+// CRUD operations for receptionists table
+//app.use('/receptionists', require('./main'))
+
+//CRUD operations for drug categories
+app.use('/drugdetails', require('./drugs.js'))
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
+
